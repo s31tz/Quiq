ce484108970810139e34046fee5c28e94800fdf3
Frank Seitz <fs@fseitz.de>
2019-05-19 17:38:28 +0200

    Savepoint

--- lib/Quiq/Ssh.pm
+++ lib/Quiq/Ssh.pm
@@ -27,7 +27,7 @@ L<Quiq::Hash>
 
 Ein Objekt der Klasse repräsentiert eine SSH-Verbindung zu einem Host.
 Über die Verbindung können Shell-Kommandos ausgeführt werden. Die
-Klasse ist ein Wrapper um die Klasse Net::SSH::Perl, die das
+Klasse ist ein Wrapper fr die Klasse Net::SSH::Perl, die das
 SSH Netzprotokoll direkt spricht.
 
 =head1 EXAMPLE
@@ -64,6 +64,10 @@ Hostname
 
 Schreibe Debug-Information über die SSH-Kommunikation nach STDERR.
 
+=item -loginShell => $bool (Default: 1)
+
+Default für Methode exec(): Führe Kommandos per Login-Shell aus.
+
 =item -user => $user (Default: I<Wert von $USER>)
 
 Name des Benutzers.
@@ -91,6 +95,7 @@ sub new {
 
     my ($argA,$opt) = Quiq::Parameters->extractToObject(\@_,1,1,
         -debug => 0,
+        -loginShell => 1,
         -user => $ENV{'USER'},
     );
     my $host = shift @$argA;
@@ -116,6 +121,7 @@ sub new {
     # Objekt instantiieren
 
     return $class->SUPER::new(
+        loginShell => $opt->loginShell,
         obj => $obj,
     );
 }
@@ -135,7 +141,7 @@ sub new {
 
 =over 4
 
-=item $cmd
+=item $cmd (String)
 
 Kommandozeile
 
@@ -145,7 +151,7 @@ Kommandozeile
 
 =over 4
 
-=item -loginShell => $bool (Default: 1)
+=item -loginShell => $bool (Default: I<Wert der Option beim Konstruktor>)
 
 Führe das Remote-Kommando unter einer Login-Shell aus. Als Shell
 wird die bash verwendet.
@@ -163,15 +169,16 @@ liefere den Exitcode als dritten Returnwert zurück.
 
 =item $stdout
 
-Ausgabe des Kommandos auf stdout.
+Ausgabe des Kommandos auf stdout (String).
 
 =item $stderr
 
-Ausgabe des Kommandos auf stderr.
+Ausgabe des Kommandos auf stderr (String).
 
 =item $exit
 
-Exitcode des Kommandos. Ist immer 0, außer wenn -sloppy=>1 gesetzt ist.
+Exitcode des Kommandos (Integer). Wird gesetzt, wenn -sloppy=>1 ist,
+sonst konstant 0.
 
 =back
 
@@ -185,7 +192,7 @@ sub exec {
 
     # Argumente und Optionen
 
-    my $loginShell = 1;
+    my $loginShell = $self->get('loginShell');
     my $sloppy = 0;
 
     my $argA = Quiq::Parameters->extractToVariables(\@_,1,1,
