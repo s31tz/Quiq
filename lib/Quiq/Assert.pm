cf461250480a027451d2deca266ae904ba58eed0
Frank Seitz <fs@fseitz.de>
2019-06-30 23:05:14 +0200

    Savepoint

--- lib/Quiq/Assert.pm
+++ lib/Quiq/Assert.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 use v5.10.0;
 
-our $VERSION = '1.148';
+our $VERSION = '1.149';
 
 use Quiq::Math;
 
@@ -313,7 +313,7 @@ sub isNumber {
 
 =head1 VERSION
 
-1.148
+1.149
 
 =head1 AUTHOR
 
