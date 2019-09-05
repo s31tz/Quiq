a74ad9af77bb25ace61679e46d6f7bd3caf2fa48
Frank Seitz <fs@fseitz.de>
2019-09-05 21:21:20 +0200

    Savepoint

--- lib/Quiq/Concat.pm
+++ lib/Quiq/Concat.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 use v5.10.0;
 
-our $VERSION = '1.156';
+our $VERSION = '1.157';
 
 # -----------------------------------------------------------------------------
 
@@ -88,7 +88,7 @@ sub catIf {
 
 =head1 VERSION
 
-1.156
+1.157
 
 =head1 AUTHOR
 
--- lib/Quiq/Epoch.pm
+++ lib/Quiq/Epoch.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 use v5.10.0;
 
-our $VERSION = '1.156';
+our $VERSION = '1.157';
 
 use Time::HiRes ();
 use Time::Local ();
@@ -189,7 +189,7 @@ sub as {
 
 =head1 VERSION
 
-1.156
+1.157
 
 =head1 AUTHOR
 
