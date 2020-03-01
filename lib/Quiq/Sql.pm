80f04635ed39daf38fa3ea4e634a55888acddf43
Frank Seitz <fs@fseitz.de>
2020-03-01 17:57:59 +0100

    Savepoint

--- lib/Quiq/Sql.pm
+++ lib/Quiq/Sql.pm
@@ -6,7 +6,7 @@ use strict;
 use warnings;
 use utf8;
 
-our $VERSION = '1.176';
+our $VERSION = '1.177';
 
 use Quiq::Hash;
 use Quiq::Option;
@@ -5389,7 +5389,7 @@ sub diff {
 
 =head1 VERSION
 
-1.176
+1.177
 
 =head1 AUTHOR
 
