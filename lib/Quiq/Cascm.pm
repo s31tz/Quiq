ba90737906114ccf58402f8586d98b8983054a41
Frank Seitz <fs@fseitz.de>
2020-04-05 10:59:40 +0200

    Savepoint

--- lib/Quiq/Cascm.pm
+++ lib/Quiq/Cascm.pm
@@ -6,7 +6,7 @@ use strict;
 use warnings;
 use utf8;
 
-our $VERSION = '1.178';
+our $VERSION = '1.179';
 
 use Quiq::Database::Row::Array;
 use Quiq::AnsiColor;
@@ -2943,7 +2943,7 @@ sub runSql {
 
 =head1 VERSION
 
-1.178
+1.179
 
 =head1 AUTHOR
 
--- lib/Quiq/Html/Component.pm
+++ lib/Quiq/Html/Component.pm
@@ -5,7 +5,7 @@ use v5.10;
 use strict;
 use warnings;
 
-our $VERSION = '1.178';
+our $VERSION = '1.179';
 
 use Quiq::Css;
 use Quiq::JavaScript;
@@ -397,7 +397,7 @@ sub putValue {
 
 =head1 VERSION
 
-1.178
+1.179
 
 =head1 AUTHOR
 
