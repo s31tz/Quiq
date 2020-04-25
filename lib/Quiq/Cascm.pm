a23c95549e04f2f814ff07330128f6d013ba4e63
Frank Seitz <fs@fseitz.de>
2020-04-25 18:32:36 +0200

    Savepoint

--- lib/Quiq/Cascm.pm
+++ lib/Quiq/Cascm.pm
@@ -6,7 +6,7 @@ use strict;
 use warnings;
 use utf8;
 
-our $VERSION = '1.180';
+our $VERSION = '1.181';
 
 use Quiq::Database::Row::Array;
 use Quiq::AnsiColor;
@@ -2929,7 +2929,7 @@ sub runSql {
 
 =head1 VERSION
 
-1.180
+1.181
 
 =head1 AUTHOR
 
