c2d428f65884c00672b66138c6d4050d6270a655
Frank Seitz <fs@fseitz.de>
2022-04-18 22:00:04 +0200

    Savepoint

--- lib/Quiq/Database/Patch.pm
+++ lib/Quiq/Database/Patch.pm
@@ -4,11 +4,62 @@
 
 =head1 NAME
 
-Quiq::Database::Patch - Definiere Datenbankpatches und wende sie an (Basisklasse)
+Quiq::Database::Patch - Definiere Patches für eine Datenbank und wende sie an (Basisklasse)
 
 =head1 BASE CLASS
 
 L<Quiq::Hash>
+
+=head1 SYNOPSIS
+
+Abgeleitete Patch-Klasse mit Patch-Methoden patch1() ...
+patchN() definieren:
+
+  package MyPatchClass;
+  use base qw/Quiq::Database::Patch/;
+  
+  sub patch1 {
+      my ($self,$db) = @_;
+  
+      # Code Patch 1
+  
+      return;
+  }
+  
+  sub patch2 {
+      my ($self,$db) = @_;
+  
+      # Code Patch 2
+  
+      return;
+  }
+  
+  # ...
+
+Ein oder mehrere Patches auf eine Datenbank anwenden:
+
+  $db = Quiq::Database::Connection->new($udl);
+  $pat = MyPatchClass->new($db);
+  $pat->apply($n);
+
+=head1 DESCRIPTION
+
+Wir entwickeln eine Datenbank, indem wir fortgesetzt Patches auf
+sie anwenden. Die Patches können Schema- oder Datenänderungen betreffen.
+Die Patches werden fortschreitend alle zusammen in einer einzigen Klasse
+definiert. Die Patchklasse ist von der Klasse Quiq::Database::Patch abgeleitet.
+Jeder Patch wird durch eine Methode mit dem Namen C<patch>I<N> realisiert.
+Hierbei ist I<N> der Patchlevel. Wir heben die Datenbank auf
+Patchlevel $n, indem wir die Methode C<< $pat->apply($n) >> aufrufen.
+Alle Patches vom aktuellen Patchlevel+1 bis $n werden dabei
+nacheinander auf die Datenbank angewandt. Ist der aktuelle Patchlevel
+gleich oder größer dem angeforderten Patchlevel $n, wird kein Patch
+angewandt. Auf einen früheren Patchlevel als den aktuellen Patchlevel
+kann nicht zurückgegangen werden. Soll ein Patch zurückgenommen werden,
+ist ein weiterer Patch zu schreiben, der diesen rückgängig macht.
+Jeder Patch wird einzeln committet. Der aktuelle Patchlevel ist in der
+Tabelle C<PATCHLEVEL> festgehalten. Diese wird beim ersten Aufruf der
+Methode C<< $class->new($db) >> automatisch angelegt.
 
 =cut
 
@@ -61,9 +112,58 @@ dieses Objekt zurück.
 sub new {
     my ($class,$db) = @_;
 
+    # Patchlevel-Tabelle anlegen, falls sie nicht existiert
+
+    if (!$db->tableExists('patchlevel')) {
+        $db->createTable('patchlevel',
+            ['pat_id',type=>'INTEGER',notNull=>1],
+        );
+        $db->insert('patchlevel',pat_id=>0);
+    }
+
+    # Instantiiere Objekt
+
     return $class->SUPER::new(
         db => $db,
     );
+}
+
+# -----------------------------------------------------------------------------
+
+=head2 Klassenmethoden
+
+=head3 maxLevel() - Liefere den höchsten definierten Patchlevel
+
+=head4 Synopsis
+
+  $level = this->maxLevel;
+
+=head4 Returns
+
+(Integer) Patchlevel
+
+=head4 Description
+
+Ermittele den höchsten definierten Patchlevel und liefere diesen zurück.
+
+=cut
+
+# -----------------------------------------------------------------------------
+
+sub maxLevel {
+    my $this = shift;
+
+    my $level = 0;
+
+    while (1) {
+        my $method = sprintf 'patch%d',$level+1;
+        if (!$this->can($method)) {
+            last;
+        }
+        $level++;
+    }
+
+    return $level;
 }
 
 # -----------------------------------------------------------------------------
@@ -97,7 +197,59 @@ Wende alle Patches an, bis Patchlevel $level erreicht ist.
 sub apply {
     my ($self,$level) = @_;
 
+    my $db = $self->db;
+
+    # Aktuellen Patchlevel ermitteln
+    my $currLevel = $self->currentLevel;
+
+    # Überprüfe die Existenz der erforderlichen Patch-Methoden
+
+    for (my $i = $currLevel+1; $i <= $level; $i++) {
+        my $method = sprintf 'patch%d',$i;
+        if (!$self->can($method)) {
+            $self->throw(
+                'PATCH-00001: Patch method does not exist',
+                Patch => $method,
+            );
+        }
+    }
+
+    # Wende Patch-Methoden an
+
+    for (my $i = $currLevel+1; $i <= $level; $i++) {
+        my $method = sprintf 'patch%d',$i;
+        say "Applying $method()...";
+        $db->begin;
+        $self->$method($db);
+        $db->update('patchlevel',pat_id=>$i);
+        $db->commit;
+    }
+
     return;
+}
+
+# -----------------------------------------------------------------------------
+
+=head3 currentLevel() - Liefere aktuellen Patchlevel
+
+=head4 Synopsis
+
+  $level = $pat->currentLevel;
+
+=head4 Returns
+
+(Integer) Patchlevel
+
+=head4 Description
+
+Ermittele den aktuellen Patchlevel und liefere diesen zurück.
+
+=cut
+
+# -----------------------------------------------------------------------------
+
+sub currentLevel {
+    return shift->db->value('patchlevel','pat_id');
 }
 
 # -----------------------------------------------------------------------------
--- t/bin/Quiq/Database/Patch.t
+++ t/bin/Quiq/Database/Patch.t
@@ -7,6 +7,7 @@ use v5.10;
 use strict;
 use warnings;
 
+use Quiq::Perl;
 use Quiq::Database::Connection;
 use Quiq::Path;
 
@@ -18,14 +19,46 @@ sub test_loadClass : Init(1) {
 
 # -----------------------------------------------------------------------------
 
-sub test_unitTest: Test(1) {
+sub test_unitTest: Test(5) {
     my $self = shift;
 
-    my $testDb = "dbi#sqlite:/tmp/test$$.db";
-    my $db = Quiq::Database::Connection->new($testDb);
+    Quiq::Perl->createClass('Test::Patch','Quiq::Database::Patch');
+    Quiq::Perl->setSubroutine('Test::Patch',patch1=>sub {
+        my ($self,$db) = @_;
+        $db->createTable('person',
+            ['per_id',type=>'INTEGER',primaryKey=>1,autoIncrement=>1],
+            ['per_vorname',type=>'STRING(20)',notNull=>1],
+        );
+        return;
+    });
+    Quiq::Perl->setSubroutine('Test::Patch',patch2=>sub {
+        my ($self,$db) = @_;
+        $db->insertMulti('person',['per_vorname'],[
+            ['Linus'],
+            ['Hanno'],
+        ]);
+        return;
+    });
 
-    my $pat = Quiq::Database::Patch->new($db);
-    $self->is(ref($pat),'Quiq::Database::Patch');
+    my $maxLevel = Test::Patch->maxLevel;
+    $self->is($maxLevel,2);
+
+    my $testDb = "dbi#sqlite:/tmp/test$$.db";
+    my $db = Quiq::Database::Connection->new($testDb,-log=>1);
+
+    my $pat = Test::Patch->new($db);
+    $self->is(ref($pat),'Test::Patch');
+
+    my $currLevel = $pat->currentLevel;
+    $self->is($currLevel,0);
+
+    $pat->apply(2);
+
+    my $tab = $db->select('person');
+    $self->is($tab->count,2);
+
+    $currLevel = $pat->currentLevel;
+    $self->is($currLevel,2);
 
     Quiq::Path->delete($testDb);
 }
