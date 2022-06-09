35f6fb1c5dde46858733c7ed64b4d219a13124ca
Frank Seitz <fs@fseitz.de>
2022-06-09 12:00:03 +0200

    Savepoint

--- lib/Quiq/Formatter.pm
+++ lib/Quiq/Formatter.pm
@@ -23,6 +23,8 @@ use warnings;
 
 our $VERSION = '1.203';
 
+use Quiq::Math;
+use Quiq::Formatter;
 use Quiq::Epoch;
 
 # -----------------------------------------------------------------------------
@@ -60,6 +62,47 @@ sub new {
 # -----------------------------------------------------------------------------
 
 =head2 Zahlen
+
+=head3 geldbetrag() - Formatiere Zahl als Geldbetrag
+
+=head4 Synopsis
+
+  $str = $class->geldbetrag($x);
+
+=head4 Arguments
+
+=over 4
+
+=item $x
+
+Zahl
+
+=back
+
+=head4 Returns
+
+(String) Zahl als Geldbetrag
+
+=head4 Description
+
+Formatiere eine Zahl $x als deutschen Geldbetrag, d.h. mit Komma
+als Dezimaltrennzeichen, zwei Nachkommastellen, Punkt als
+Tausender-Trennzeichen und liefere das Resultat zurück.
+
+=cut
+
+# -----------------------------------------------------------------------------
+
+sub geldbetrag {
+    my ($class,$x) = @_;
+
+    $x = Quiq::Math->roundTo($x,2);
+    $x = Quiq::Formatter->readableNumber($x);
+
+    return $x;
+}
+
+# -----------------------------------------------------------------------------
 
 =head3 normalizeNumber() - Normalisiere Zahldarstellung
 
