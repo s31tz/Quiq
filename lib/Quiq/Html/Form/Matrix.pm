b36bb41a71d948a1b430b0020977f0a66b887a35
Frank Seitz <fs@fseitz.de>
2022-07-17 22:00:04 +0200

    Savepoint

---
+++ lib/Quiq/Html/Form/Matrix.pm
@@ -0,0 +1,180 @@
+# -----------------------------------------------------------------------------
+
+=encoding utf8
+
+=head1 NAME
+
+Quiq::Html::Form::Matrix - HTML-Formular mit Matrix-Layout
+
+=head1 BASE CLASS
+
+L<Quiq::Hash>
+
+=head1 DESCRIPTION
+
+Die Klasse erzeugt ein HTML-Formular mit Matrix-Layout,
+d.h. es besteht aus mehreren Zeilen gleichartiger Widgets.
+
+=head1 ATTRIBUTES
+
+=over 4
+
+=item initialize => $sub (Default: sub {})
+
+Subroutine zur Initialisierung der Widgets. Beispiel:
+
+  sub {
+      my ($w,$name,$i) = @_;
+  
+      my $val = $self->param($name."_$i");
+      $w->value($val);
+  }
+
+=item names => \@names (Default: [])
+
+Liste der Widgetnamen. Zum diesen Widgetnamen wird jeweils "_$i"
+mit der Nummer $i der Zeile hinzugefügt, beginnend mit 1 für die
+erste Zeile.
+
+=item rows => $n (Default: 1)
+
+Anzahl der Zeilen.
+
+=item titles => \@titles (Default: [])
+
+Liste der Kolumnentitel.
+
+=item widgets => \@widgets (Default: [])
+
+Widgets einer Matrix-Zeile.
+
+=back
+
+=cut
+
+# -----------------------------------------------------------------------------
+
+package Quiq::Html::Form::Matrix;
+use base qw/Quiq::Hash/;
+
+use v5.10;
+use strict;
+use warnings;
+
+our $VERSION = '1.204';
+
+use Quiq::Storable;
+use Quiq::Html::Table::Simple;
+
+# -----------------------------------------------------------------------------
+
+=head1 METHODS
+
+=head2 Konstruktor
+
+=head3 new() - Instantiiere Objekt
+
+=head4 Synopsis
+
+  $e = $class->new(@keyVal);
+
+=head4 Description
+
+Instantiiere ein Formular-Objekt mit den Eigenschaften @keyVal und
+liefere eine Referenz auf dieses Objekt zurück.
+
+=cut
+
+# -----------------------------------------------------------------------------
+
+sub new {
+    my $class = shift;
+    # @_: @keyVal
+
+    my $self = $class->SUPER::new(
+        initialize => sub {},
+        names => [],
+        rows => 1,
+        titles => [],
+        widgets => [],
+    );
+    $self->set(@_);
+
+    return $self;
+}
+
+# -----------------------------------------------------------------------------
+
+=head2 Objektmethoden
+
+=head3 html() - Generiere HTML
+
+=head4 Synopsis
+
+  $html = $e->html($h);
+  $html = $class->html($h,@keyVal);
+
+=head4 Description
+
+Generiere den HTML-Code der Formular-Matrix und liefere diesen
+zurück. Als Klassenmethode gerufen, wird das Objekt intern erzeugt
+und mit den Attributen @keyVal instantiiert.
+
+=cut
+
+# -----------------------------------------------------------------------------
+
+sub html {
+    my $this = shift;
+    my $h = shift;
+
+    my $self = ref $this? $this: $this->new(@_);
+
+    my ($initializeSub,$rows,$titleA,$widgetA) =
+        $self->get(qw/initialize rows titles widgets/);
+
+    my @rows;
+    for my $i (1 .. $rows) {
+        my @row = ($i);
+        for (@$widgetA) {
+            my $w = Quiq::Storable->clone($_);
+            my $name = $w->name."_$i";
+            $w->name($name);
+            $initializeSub->($w,$name,$i);
+            push @row,$w;
+            push @rows,\@rows;
+        }
+    }
+
+    return Quiq::Html::Table::Simple->html($h,
+        border => 1,
+        rows => \@rows,
+    );
+}
+
+# -----------------------------------------------------------------------------
+
+=head1 VERSION
+
+1.204
+
+=head1 AUTHOR
+
+Frank Seitz, L<http://fseitz.de/>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2022 Frank Seitz
+
+=head1 LICENSE
+
+This code is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+# -----------------------------------------------------------------------------
+
+1;
+
+# eof
