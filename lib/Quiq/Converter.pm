5be2f8f889c18421bd532744fb374267552b7c4d
Frank Seitz <fs@fseitz.de>
2022-11-13 18:00:03 +0100

    Savepoint

--- lib/Quiq/Converter.pm
+++ lib/Quiq/Converter.pm
@@ -153,6 +153,50 @@ sub snakeCaseToCamelCase {
 
 # -----------------------------------------------------------------------------
 
+=head3 protectRegexChars() - Maskiere Regex-Metazeichen in Zeichenkette
+
+=head4 Synopsis
+
+  $strProtexted = $this->protectRegexChars($str);
+
+=head4 Arguments
+
+=over 4
+
+=item $str
+
+Zeichenkette, in der die Zeichen maskiert werden sollen
+
+=back
+
+=head4 Returns
+
+(String) Zeichenkette, in der die Regex-Metazeichen maskiert sind.
+
+=head4 Description
+
+Maskiere in Zeichenkete $str alle Regex-Metazeichen und liefere
+das Resultat zurück. Maskiert werden die Zeichen
+
+  . + * ? | ( ) { } [ ] \ ^ $
+
+Im Gegensatz zur Perl Buildinfunktion quotemeta() bzw. "\Q..."
+maskiert diese Methode keine anderen Zeichen als Regex-Metazeichen.
+
+=cut
+
+# -----------------------------------------------------------------------------
+
+sub protectRegexChars {
+    my ($this,$str) = @_;
+
+    $str =~ s/([.+*?|(){}\[\]\\^\$])/\\$1/g;
+
+    return $str;
+}
+
+# -----------------------------------------------------------------------------
+
 =head3 strToHex() - Wandele String in Hex-Darstellung
 
 =head4 Synopsis
