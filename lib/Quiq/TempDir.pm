84a517383e9d862544f09f05c4ecbeb4fb285814
Frank Seitz <fs@fseitz.de>
2023-01-17 23:12:15 +0100

    Savepoint

--- lib/Quiq/TempDir.pm
+++ lib/Quiq/TempDir.pm
@@ -31,6 +31,7 @@ use warnings;
 our $VERSION = '1.207';
 
 use overload '""' => sub {${$_[0]}}, 'cmp' => sub{${$_[0]} cmp $_[1]};
+use Quiq::Path;
 use File::Temp ();
 
 # -----------------------------------------------------------------------------
@@ -54,6 +55,10 @@ use File::Temp ();
 Entferne das Verzeichnis bei Beendigung des Programms. Wenn 0, bleibt das
 Verzeichnis nach Beendigung des Programms bestehen.
 
+=item -parentDir => $dir
+
+Erzeuge das temporäre Verzeichnis unterhalb von Verzeichnis $dir.
+
 =back
 
 =head4 Returns
@@ -76,9 +81,11 @@ sub new {
     # Optionen und Argumente
 
     my $cleanup = 1;
+    my $parentDir = undef;
 
     my $argA = $class->parameters(0,0,\@_,
         -cleanup => \$cleanup,
+        -parentDir => \$parentDir,
     );
 
     # Wir setzen unsere Optionen in die Optionen von File::Temp::newdir() um
@@ -86,6 +93,9 @@ sub new {
     my @args;
     if (defined $cleanup) {
         push @args,'CLEANUP',$cleanup;
+    }
+    if ($parentDir) {
+        push @args,'DIR',Quiq::Path->expandTilde($parentDir);
     }
 
     # Objekt instantiieren
