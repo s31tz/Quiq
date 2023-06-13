c5542f0af78e15e1454c801cc860b4d2960320f1
Frank Seitz <fs@fseitz.de>
2023-06-13 22:00:04 +0200

    Savepoint

--- lib/Quiq/PhotoStorage.pm
+++ lib/Quiq/PhotoStorage.pm
@@ -52,14 +52,15 @@ our $VERSION = '1.210';
 use Quiq::Path;
 use Quiq::LockedCounter;
 use Quiq::Hash::Db;
+use Quiq::Shell;
 
 # -----------------------------------------------------------------------------
 
 =head1 METHODS
 
-=head2 Konstruktor
+=head2 Klassenmethoden
 
-=head3 new() - Instantiiere Objekt
+=head3 new() - Konstruktor
 
 =head4 Synopsis
 
@@ -78,20 +79,79 @@ sub new {
 
     my $p = Quiq::Path->new;
 
-    if (!$p->exists($dir)) {
-        $p->mkdir($dir,-recursive=>1);
-        $p->mkdir("$dir/pic");
-        
-        say "Photo Storage $dir created";
-    }
+    # $dir muss existieren
+    $p->mkdir("$dir/pic");
     my $cnt = Quiq::LockedCounter->new("$dir/cnt.txt");
-    my $hsh = Quiq::Hash::Db->new("$dir/sha1.hash",'rw');
+    my $h = Quiq::Hash::Db->new("$dir/sha1.hash",'rw');
 
     return $class->SUPER::new(
         dir => $dir,
         cnt => $cnt,
-        hsh => $hsh,
+        h => $h,
     );
+}
+
+# -----------------------------------------------------------------------------
+
+=head2 Objektmethoden
+
+=head3 add() - Füge Fotodatei hinzu
+
+=head4 Synopsis
+
+  $name = $pst->add($file);
+
+=head4 Description
+
+Füge Fotodatei $file zum Speicher hinzu und liefere den Basisnamen
+der Datei zurück.
+
+=cut
+
+# -----------------------------------------------------------------------------
+
+sub add {
+    my ($self,$file) = @_;
+
+    my ($dir,$cnt,$h) = $self->get(qw/dir cnt h/);
+
+
+    my $p = Quiq::Path->new;
+    my $sha1 = $p->sha1($file);
+
+    if ($h->get($sha1)) {
+        $p->delete($file);
+        return ''; # Bild ist bereits im Speicher
+    }
+
+    my $basename = $p->basename($file);
+    my $ext = lc $p->extension($file);
+    if ($ext eq 'jpg' || $ext eq 'jpeg') {
+        # Bild ist JPEG
+    }
+    elsif ($ext eq 'png' || $ext eq 'webp') {
+        # Bilddatei nach JPEG wandeln
+        
+        my $sh = Quiq::Shell->new;
+        $sh->exec("convert '$file' '$file.jpg'");
+        $p->delete($file);
+        $file = "$file.jpg";
+    }
+    else {
+        $self->throw(
+            'PHOTOSTORAGE-00099: Unknown file format',
+            File => $file,
+        );
+    }
+
+    my $n = $cnt->increment->count;
+    $basename =~ s/[^-_a-zA-Z0-9]/_/g;
+    my $destFile = sprintf '%s/pic/%07d-%s.jpg',$dir,$n,$basename;
+    $p->duplicate('move',$file,$destFile);
+    say $destFile;
+    $h->{$sha1}++;
+
+    return;
 }
 
 # -----------------------------------------------------------------------------
