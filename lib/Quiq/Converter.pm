39df8d7b8567f7fe47a1819141f145ed898f7424
Frank Seitz <fs@fseitz.de>
2023-08-18 18:00:03 +0200

    Savepoint

--- lib/Quiq/Converter.pm
+++ lib/Quiq/Converter.pm
@@ -22,7 +22,7 @@ use strict;
 use warnings;
 use utf8;
 
-our $VERSION = '1.211';
+our $VERSION = '1.212';
 
 use POSIX ();
 use Time::Local ();
@@ -754,7 +754,7 @@ sub stringToKeyVal {
 
 =head1 VERSION
 
-1.211
+1.212
 
 =head1 AUTHOR
 
--- lib/Quiq/PhotoStorage.pm
+++ lib/Quiq/PhotoStorage.pm
@@ -52,7 +52,7 @@ use v5.10;
 use strict;
 use warnings;
 
-our $VERSION = '1.211';
+our $VERSION = '1.212';
 
 use Quiq::Path;
 use Quiq::LockedCounter;
@@ -207,7 +207,7 @@ sub addAllByTime {
 
 =head1 VERSION
 
-1.211
+1.212
 
 =head1 AUTHOR
 
