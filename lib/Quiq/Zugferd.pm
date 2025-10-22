7f6a4a01dfd8c2fe1292f5c9bea331ba5e6268af
Frank Seitz <fs@fseitz.de>
2025-10-22 12:00:01 +0200

    Savepoint

--- lib/Quiq/Zugferd.pm
+++ lib/Quiq/Zugferd.pm
@@ -42,7 +42,7 @@ use strict;
 use warnings;
 use utf8;
 
-our $VERSION = '1.231';
+our $VERSION = '1.232';
 
 use Quiq::PerlModule;
 use Quiq::Path;
@@ -1269,7 +1269,7 @@ Texte auf Rechnungs- und Positionsebene
 
 =head1 VERSION
 
-1.231
+1.232
 
 =head1 AUTHOR
 
